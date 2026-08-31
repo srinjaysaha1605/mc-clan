538f27dafce18bbaab1cdeef9c25e9411880cd5a
Srinjay <srinjaysaha1605@gmail.com>
2026-08-31 15:11:06 +0530

    Add index parameter to player mapping in AdminDashboard

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -98,7 +98,7 @@ export const AdminDashboard = ({ players, onLogout, onViewSite }: AdminDashboard
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {players.map((player) => (
+          {players.map((player, index) => (
             <div
               key={player.id}
               className="border border-white/10 bg-white/5 p-6 flex flex-col gap-4"
